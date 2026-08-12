fc32cbd7daf3808a15169623b1933c39d040c428
k-mama <emmaestro123@gmail.com>
2026-08-12 23:24:35 +1000

    Make homepage hero-only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-import BrandStatement from "@/components/home/BrandStatement";
-import CuratedWorlds from "@/components/home/CuratedWorlds";
-import StudioMethod from "@/components/home/StudioMethod";
-import Contact from "@/components/home/Contact";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
-      <BrandStatement />
-      <CuratedWorlds />
-      <StudioMethod />
-      <Contact />
-      <Footer />
     </main>
   );
 }
