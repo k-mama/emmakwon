1cbfe680d699bbe8765874df9eb366a371e52efd
k-mama <emmaestro123@gmail.com>
2026-08-15 02:43:54 +1000

    Keep site header outside homepage main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <Hero />
-    </main>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
