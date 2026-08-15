4426c4552b28b59b1cb41de93149e2455f7b8691
k-mama <emmaestro123@gmail.com>
2026-08-15 22:59:21 +1000

    Show optional cover images in Studio Notes list

--- src/components/studio/NoteList.tsx
+++ src/components/studio/NoteList.tsx
@@ -7,11 +7,20 @@ export default function NoteList({ posts }: { posts: StudioPost[] }) {
     <ul className={styles.list}>
       {posts.map((post) => (
         <li key={post.slug} className={styles.item}>
-          <Link href={`/studio/notes/${post.slug}/`} className={styles.link}>
+          <Link
+            href={`/studio/notes/${post.slug}/`}
+            className={`${styles.link} ${post.coverImage ? styles.linkWithCover : ""}`}
+          >
             <span className={styles.category}>{post.category}</span>
             <span className={styles.title}>{post.title}</span>
             <span className={styles.excerpt}>{post.excerpt}</span>
             <span className={styles.date}>{formatStudioDate(post.publishedAt ?? post.createdAt)}</span>
+            {post.coverImage ? (
+              <span className={styles.thumb} aria-hidden="true">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={post.coverImage} alt="" loading="lazy" />
+              </span>
+            ) : null}
           </Link>
         </li>
       ))}
