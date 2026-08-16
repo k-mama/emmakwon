942c34f9147f02aec5b453567056dbdf6e54e438
k-mama <emmaestro123@gmail.com>
2026-08-16 12:29:43 +1000

    Make robots metadata route static for export

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const SITE_URL = "https://emmakwon.pages.dev";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
