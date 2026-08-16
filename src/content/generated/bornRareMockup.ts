544b107528e24feb5f56b58424cdd0c60648bcfc
k-mama <emmaestro123@gmail.com>
2026-08-16 13:20:01 +1000

    Use ivory BORN RARE mockup in books hero

--- src/content/generated/bornRareMockup.ts
+++ src/content/generated/bornRareMockup.ts
@@ -1,8 +1,8 @@
-import part1 from "./bornRareMockupPart1";
-import part2 from "./bornRareMockupPart2";
-import part3 from "./bornRareMockupPart3";
-import part4 from "./bornRareMockupPart4";
+import part1 from "./bornRareMockupIvoryPart1";
+import part2 from "./bornRareMockupIvoryPart2";
+import part3 from "./bornRareMockupIvoryPart3";
+import part4 from "./bornRareMockupIvoryPart4";
 
-// Generated from the approved BORN RARE 3D mockup. Kept as a compact
+// Generated from the approved ivory-diary BORN RARE 3D mockup. Kept as a compact
 // build-time data URI because the repository connector writes text assets.
 export const bornRareMockupSrc = `data:image/webp;base64,${part1}${part2}${part3}${part4}`;
