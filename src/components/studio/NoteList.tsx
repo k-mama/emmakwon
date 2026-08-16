50dd405645e3cd21b10b750af1a9495e0c18cf42
k-mama <emmaestro123@gmail.com>
2026-08-16 11:51:36 +1000

    Use semantic dates in Studio note lists

--- src/components/studio/NoteList.tsx
+++ src/components/studio/NoteList.tsx
@@ -5,25 +5,31 @@ import styles from "./NoteList.module.css";
 export default function NoteList({ posts }: { posts: StudioPost[] }) {
   return (
     <ul className={styles.list}>
-      {posts.map((post) => (
-        <li key={post.slug} className={styles.item}>
-          <Link
-            href={`/studio/notes/${post.slug}/`}
-            className={`${styles.link} ${post.coverImage ? styles.linkWithCover : ""}`}
-          >
-            <span className={styles.category}>{post.category}</span>
-            <span className={styles.title}>{post.title}</span>
-            <span className={styles.excerpt}>{post.excerpt}</span>
-            <span className={styles.date}>{formatStudioDate(post.publishedAt ?? post.createdAt)}</span>
-            {post.coverImage ? (
-              <span className={styles.thumb} aria-hidden="true">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={post.coverImage} alt="" loading="lazy" />
-              </span>
-            ) : null}
-          </Link>
-        </li>
-      ))}
+      {posts.map((post) => {
+        const dateValue = post.publishedAt ?? post.createdAt;
+
+        return (
+          <li key={post.slug} className={styles.item}>
+            <Link
+              href={`/studio/notes/${post.slug}/`}
+              className={`${styles.link} ${post.coverImage ? styles.linkWithCover : ""}`}
+            >
+              <span className={styles.category}>{post.category}</span>
+              <span className={styles.title}>{post.title}</span>
+              <span className={styles.excerpt}>{post.excerpt}</span>
+              <time className={styles.date} dateTime={dateValue}>
+                {formatStudioDate(dateValue)}
+              </time>
+              {post.coverImage ? (
+                <span className={styles.thumb} aria-hidden="true">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img src={post.coverImage} alt="" loading="lazy" />
+                </span>
+              ) : null}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
